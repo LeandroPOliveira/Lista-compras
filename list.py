d5c61fc91c48f881e5dc3d6cc728ad4165a26b7e
Leandro Oliveira <leandro_lpo2@hotmail.com>
2023-02-18 11:45:43 -0300

    Ajuste tela inicial

--- list.py
+++ list.py
@@ -1,81 +1,78 @@
 import math
 import sqlite3
-
 from kivy.config import Config
 from kivy.uix.screenmanager import Screen
-
-
 Config.set('graphics', 'resizable', '1')
 Config.set('graphics', 'width', '389')
 Config.set('graphics', 'height', '700')
 from kivy.lang import Builder
 from kivymd.uix.textfield import MDTextField
-from kivy.properties import StringProperty, Clock
+from kivy.properties import Clock
 from kivymd.app import MDApp
-from kivymd.uix.list import IRightBodyTouch, OneLineAvatarIconListItem, TwoLineAvatarIconListItem, ImageRightWidget, \
-    IconLeftWidget, OneLineListItem, IconRightWidget
-from kivymd.uix.selectioncontrol import MDCheckbox
-from kivymd.icon_definitions import md_icons
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.datatables import MDDataTable
-import random
-from functools import partial
 from kivy.metrics import dp
 from kivy.utils import get_color_from_hex
+
 
 class Principal(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.entrada = None
+        self.dados_listagem = None
+        self.completos = None
+        self.icones = None
         self.lista = []
-        self.nomes = ['ze', 'bastio', 'mane']
+        self.produtos = []
         self.dialog = None
         Clock.schedule_once(self.carregar_lista)
 
     def carregar_lista(self, dt):
+        self.lista.clear(), self.produtos.clear()
         conn = sqlite3.connect('lista_compras')
         cursor = conn.cursor()
-
         cursor.execute('SELECT * FROM lista')
-        self.produtos = []
         self.lista = cursor.fetchall()
-        # self.completos = []
         for item in self.lista:
             self.produtos.append(item[1])
         self.icones = ['X'] * len(self.produtos)
         self.completos = list(zip(self.produtos, self.icones))
-        self.dados_listagem = MDDataTable(pos_hint={'x': 0.1, 'y': 0.2},
-                                          size_hint=(.8, .7),
+        self.dados_listagem = MDDataTable(pos_hint={'x': 0.15, 'y': 0.2},
+                                          size_hint=(.7, .6),
                                           rows_num=len(self.completos),
-                                          background_color_header=get_color_from_hex("#0bbd6d"),
-                                          background_color_selected_cell=get_color_from_hex("#d2f7e7"),
+                                          background_color_header=get_color_from_hex("#ebf52a"),
+                                          background_color_selected_cell=get_color_from_hex("#f5f7cd"),
                                           check=True,
-                                          column_data=[("[color=#ffffff]Produto[/color]", dp(50)),
-                                                       ("[color=#FFA07A][/color]", dp(10))],
+                                          column_data=[("[color=#0d0d0d]Produto[/color]", dp(40)),
+                                                       ("[color=#0d0d0d][/color]", dp(10))],
                                           row_data=self.completos, elevation=1)
 
         self.ids.scroll.add_widget(self.dados_listagem)
         self.dados_listagem.bind(on_row_press=self.on_row_press)
         self.dados_listagem.bind(on_check_press=self.on_check_press)
-    def on_row_press(self, current_row, instance_row):
-        '''Called when a table row is clicked.'''
-        print(instance_row.index)
 
+    def on_row_press(self, instance_table, instance_row):
+        # instance_row.ids.check.state = 'down'
         if instance_row.index % 2 != 0:
             self.remover(instance_row.index)
         else:
             pass
 
     def on_check_press(self, instance_table, current_row):
-        '''Called when the check box in the table row is checked.'''
-
-        print(instance_table, current_row)
+        conn = sqlite3.connect('lista_compras')
+        cursor = conn.cursor()
+        print(self.dados_listagem.get_row_checks())
+        print(current_row[0])
+        cursor.execute('DELETE FROM lista WHERE produto = (?)', (current_row[0],))
+        cursor.execute('INSERT INTO lista(produto) VALUES(?)', (current_row[0],))
+        conn.commit()
+        self.carregar_lista(dt=None)
 
     def novo_item(self):
         conn = sqlite3.connect('lista_compras')
         cursor = conn.cursor()
-
         self.entrada = MDTextField()
         self.dialog = MDDialog(
             title="Novo item:",
@@ -107,9 +104,7 @@ class Principal(Screen):
         # print(entrada.text)
         conn = sqlite3.connect('lista_compras')
         cursor = conn.cursor()
-
         cursor.execute('INSERT INTO lista(produto) VALUES(?)', (entrada.text,))
-
         conn.commit()
         self.ids.scroll.clear_widgets()
         self.carregar_lista(dt=None)
